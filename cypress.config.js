1416cb0673234648d45c116fd3a179e7b7c6586b
Badshah Khan <baasha303@gmail.com>
2024-06-09 15:44:18 +0530

    uploading reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl:"https://nf-staging.vegrow.in",
+    video:true,
+    screenshotOnRunFailure:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
